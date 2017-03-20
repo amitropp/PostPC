9bd7ffc1ac21a3b070d54cf3017571c153ab9994
amitropp <amitropp@gmail.com>
2017-03-20 23:36:37 +0200

    delete work

---
+++ MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.amitropp.todolistmanager;
+
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.util.ArrayList;
+import 	java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+    private boolean color;
+    private AlertDialog alertDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // true = red, false = blue
+        color = true;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        readItems();
+
+        /*itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                TextView v = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.my_text_view, parent, false);
+                if(items.get(position) != null )
+                if (color == true) {
+                    lvItems.setBackgroundColor(Color.RED);
+                } else {
+                    lvItems.setBackgroundColor(Color.YELLOW);
+                }
+                color = !color;
+
+                ViewHolder vh = new ViewHolder(v);
+                return vh;
+            }
+        };*/
+
+
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        // Setup remove listener method call
+        setupListViewListener();
+
+    }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        final int index = pos;
+                        CharSequence cs = "Delete";
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle(items.get(pos).toString()) //
+                                .setPositiveButton(cs, new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        // Remove the item within array at position
+                                        items.remove(index);
+                                        // Refresh the adapter
+                                        itemsAdapter.notifyDataSetChanged();
+                                        writeItems();
+                                        // Return true consumes the long click event (marks it handled)
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                        builder.show();
+                        return true;
+                    }
+
+
+                });
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.enterNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+
+        etNewItem.setText("");
+        writeItems();
+        scrollMyListViewToBottom();
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void scrollMyListViewToBottom() {
+        lvItems.post(new Runnable() {
+            @Override
+            public void run() {
+                // Select the last row so it will scroll into view...
+                lvItems.setSelection(itemsAdapter.getCount() - 1);
+            }
+        });
+    }
+}
+
